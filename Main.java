9ca53db75100aa1b9db1cdf71fcf4ceebd1de309
pnidhi06 <149089407+pnidhi06@users.noreply.github.com>
2023-10-26 14:32:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String [] args){
         System.out.println("Hello");
-        System.out.println("Will you work?");
 
         System.out.println("No");
 
