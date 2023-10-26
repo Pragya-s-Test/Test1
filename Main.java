299783ad25f18252ac982e6c2cf91c0602c42b3c
Pragya06Nidhi <nidhi.pragya06@gmail.com>
2023-10-26 12:57:22 -0400

    Why

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String [] args){
         System.out.println("Hello");
-        System.out.println("Trying to push to Main branch");
-        System.out.println("ARGGG");
-        System.out.println("God has forsaken me");
-        System.out.println("Send Help");
+        
+
+
     }
 }
